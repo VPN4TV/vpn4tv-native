3ce741ab4d60f8879682b483fc481a3b71637762
Aleksei Chertanovskii <chertanovsky@gmail.com>
2026-04-11 13:55:03 -0300

    v5.0.0 (50002): phone layout fix

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -30,7 +30,7 @@ android {
         applicationId = "com.vpn4tv.hiddify"
         minSdk = 23
         targetSdk = 36
-        versionCode = 50001
+        versionCode = 50002
         versionName = "5.0.0"
         base.archivesName.set("VPN4TV-Native-${versionName}")
     }
